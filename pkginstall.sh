abf0170adebd9d9aaa8c7ce9a9e4ccb0f0067f40
Opiture <67560905+Opiture@users.noreply.github.com>
2022-05-12 23:29:24 +1200

    Update pkginstall.sh

--- pkginstall.sh
+++ pkginstall.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 
 sudo pacman -S qtile thunar alacritty network-manager-applet python-pip ttf-font-awesome calcurse bottom lxappearance rofi feh &
-
-yay -S brave-bin picom-git ly &
-
-pip install psutil &
